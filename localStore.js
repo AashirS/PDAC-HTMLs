a198c83251b89c391d21738d6b67956c1d616763
Aashir sayed <86831004+AashirS@users.noreply.github.com>
2021-12-28 03:55:11 -0800

    PDAC HTMLs

---
+++ localStore.js
@@ -0,0 +1,7 @@
+// window.localStorage.setItem("pata", "nizam colony");
+
+// console.log(localStorage("pata"));
+
+window.sessionStorage.setItem("colony", "naka");
+
+console.log(sessionStorage("colony"));
---
+++ localstorage.js
@@ -0,0 +1,4 @@
+localStorage.setItem('email','saqebk619@gmail.com')
+localStorage.setItem('email','saqebk619@gmail.com')
+
+console.log(localStorage.setItem('email'))
